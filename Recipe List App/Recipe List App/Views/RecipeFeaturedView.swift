a289c330177f8d0d472f64fa49d8df27a7ab0eca
廖哲澄 <liaozhedeng@liaozhedengdeMacBook-Pro.local>
2021-11-16 10:59:28 +0800

    finish Featured View

--- Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
@@ -12,12 +12,41 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Featured Recipes").fontWeight(.bold).font(.largeTitle).padding(.leading).padding(.top, 40)
+            GeometryReader { geo in
+                        TabView {
+                            ForEach (0..<model.recipes.count) {
+                                index in
+                                if model.recipes[index].featured == true {
+                                    ZStack {
+                                        Rectangle().foregroundColor(.white)
+                                        VStack(spacing:0) {
+                                            Image(model.recipes[index].image).resizable().clipped().aspectRatio( contentMode: .fill)
+                                            Text(model.recipes[index].name).padding(5)
+                                        }
+                                    }.frame(width: geo.size.width - 40, height: geo.size.height - 100).cornerRadius(10).shadow(color:.black, radius: 10, x:-5, y:5)
+                                }
+                            }
+                        }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                    }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Country:").font(.headline)
+                Text("Peru")
+                Text("Preference Food:").font(.headline)
+                Text("Grass")
+            }.padding(.leading)
+                .padding([.leading, .bottom])
+            
+        }
+        
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeFeaturedView()
+        RecipeFeaturedView().environmentObject(RecipeModel())
     }
 }
