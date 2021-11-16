289c2a3ef9c4b6680e8ab376a2c6b8761cb42139
廖哲澄 <liaozhedeng@liaozhedengdeMacBook-Pro.local>
2021-11-16 11:35:25 +0800

    change List to ScrollView

--- Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
@@ -15,22 +15,22 @@ struct RecipeFeaturedView: View {
         VStack(alignment: .leading) {
             Text("Featured Recipes").fontWeight(.bold).font(.largeTitle).padding(.leading).padding(.top, 40)
             GeometryReader { geo in
-                        TabView {
-                            ForEach (0..<model.recipes.count) {
-                                index in
-                                if model.recipes[index].featured == true {
-                                    ZStack {
-                                        Rectangle().foregroundColor(.white)
-                                        VStack(spacing:0) {
-                                            Image(model.recipes[index].image).resizable().clipped().aspectRatio( contentMode: .fill)
-                                            Text(model.recipes[index].name).padding(5)
-                                        }
-                                    }.frame(width: geo.size.width - 40, height: geo.size.height - 100).cornerRadius(10).shadow(color:.black, radius: 10, x:-5, y:5)
+                TabView {
+                    ForEach (0..<model.recipes.count) {
+                        index in
+                        if model.recipes[index].featured == true {
+                            ZStack {
+                                Rectangle().foregroundColor(.white)
+                                VStack(spacing:0) {
+                                    Image(model.recipes[index].image).resizable().clipped().aspectRatio( contentMode: .fill)
+                                    Text(model.recipes[index].name).padding(5)
                                 }
-                            }
-                        }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                            }.frame(width: geo.size.width - 40, height: geo.size.height - 100).cornerRadius(10).shadow(color:.black, radius: 10, x:-5, y:5)
+                        }
                     }
+                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
             
             VStack(alignment: .leading, spacing: 10) {
                 Text("Country:").font(.headline)
--- Recipe List App/Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Recipe List App/Views/RecipeListView.swift
@@ -12,26 +12,39 @@ struct RecipeListView: View {
     // Reference the view model
     @EnvironmentObject var model:RecipeModel
     var body: some View {
-                NavigationView {
-                    List(model.recipes) { r in
-                        
-                        NavigationLink(
-                            destination: RecipeDetailView(recipe:r),
-                            label: {
-                                
-                                // MARK: Row item
-                                HStack(spacing: 20.0) {
-                                    Image(r.image)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 50, height: 50, alignment: .center)
-                                        .clipped()
-                                        .cornerRadius(5)
-                                    Text(r.name)
-                                }
-                                
-                            })
-                    }.navigationBarTitle("All Recipes")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("All Recipes").fontWeight(.bold).font(.largeTitle).padding(.top, 40)
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe:r),
+                                label: {
+                                    
+                                    // MARK: Row item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name).foregroundColor(.black)
+                                    }
+                                    
+                                })
+                        }
+                    }
+                    
+                }
+                
+                
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
+            
         }
         
     }
@@ -39,6 +52,6 @@ struct RecipeListView: View {
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
